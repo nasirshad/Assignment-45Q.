9b6b2368f3b2f65531cdb2e12b0ba7b4a80474c8
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 12:32:46 +0500

    Assignment-45Q

---
+++ Q4.js
@@ -0,0 +1,9 @@
+"use strict";
+// var nam : string = "MY Name Is Nasir Shad";
+//console.log(nam);
+//Question No.4
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+let author = "'Allama Muhammad Iqbal'";
+let quote = "'Nations are born in the hearts of poets,they prosper and die in the hands of politicians.'";
+console.log(author, ", Once Said, ", quote);
