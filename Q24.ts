ab7fc45dcdcc5c3c4b4fb3f8011ff902ed442ace
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 22:02:01 +0500

    Assignment-45Q

---
+++ Q24.ts
@@ -0,0 +1,22 @@
+let sq : string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+console.log(sq);
+
+// Question No.24
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 23, and write an if-else chain.
+// 	• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the 	alien.
+// 	• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+// 	• Write one version of this program that runs the if block and another that runs the else block.
+//version 1
+
+let alienColor:string="Red"
+if (alienColor=="green")
+{
+    console.log("The Players has Earned 5 Points")    
+    console.log("*********")    
+}
+//versioin 2
+else
+{
+    console.log("The Players has Earned 10 Points") 
+    console.log("++++++++")     
+}
