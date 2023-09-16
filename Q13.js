aca5ef05a2102d9618d3c2f6d957db06d83df283
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 20:55:34 +0500

    Assignment-45Q

---
+++ Q13.js
@@ -0,0 +1,10 @@
+"use strict";
+// let nam : string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+// console.log(nam);
+//Question No.13
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let listofDinner = ["Huzaifa Sb,", "Sajid Ramay Sb,", "Ahmad Sb,", "Fareed Khan Sb,"];
+for (let index = 0; index < listofDinner.length; index++) {
+    const element = listofDinner[index];
+    console.log("Hello", element, "I am pleased to invite you for Dinner 10Pm tonight ");
+}
