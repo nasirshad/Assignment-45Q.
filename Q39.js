87a0ac045c0e3f311475d847383dc0784df65c26
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 22:54:55 +0500

    Assignment-45Q

---
+++ Q39.js
@@ -0,0 +1,7 @@
+"use strict";
+// 39.	Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+let magician_Names = ["Jackal", "neelam Jajjaa", "Saddhu", "Bangali Baba"];
+function show_magicians(names) {
+    console.log(names);
+}
+show_magicians(magician_Names);
---
+++ Q39.ts
@@ -0,0 +1,7 @@
+// 39.	Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+let magician_Names:string[]=["Jackal","neelam Jajjaa","Saddhu","Bangali Baba"]
+function show_magicians(names:string[])
+{
+        console.log(names)
+}
+show_magicians(magician_Names)
