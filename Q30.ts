e9568de8d07c51a92b6c9088a0d279ceba09156d
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 22:31:05 +0500

    Assignment-45Q

---
+++ Q30.ts
@@ -0,0 +1,29 @@
+// let saq: string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+// console.log(saq);
+
+// 30.	Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+
+let current_users: String[] = ["Shoaib Akhtar", "Zaman Khan", "jeena", "stylo", "batista"]
+let new_users: String[] = ["saqi", "Wasi", "vijy", "Jamshaid", "Kojy", "Stylo"]
+
+function arraysAreEqual1(arry1: any[], arry2: any[]): any {
+    for (let i = 0; i < arry1.length; i++) {
+       var element = arry1[i]
+        for (let j = 0; j < arry2.length; j++) {
+             var elements = arry2[i];
+            if (arry1[i] == arry2[j]) {
+                console.log( "you need to find a new User Name")
+            }
+            else {
+                console.log("The User_name is available")
+            }
+
+        }
+    }
+}
+
+ arraysAreEqual1(new_users, current_users);
