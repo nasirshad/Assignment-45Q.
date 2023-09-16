d6d2c698f2dfb0b3013e1019252ee84d72a72fc7
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 20:43:26 +0500

    Assignment-45Q

---
+++ Q12.js
@@ -0,0 +1,11 @@
+"use strict";
+//et nam : string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+//console.log(nam);
+//Question No.12
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+let translist = ["TriCycle", "BiCycle", "bike", "scooty", "Car",];
+console.log("I love", translist[0], "Riding, its always amazing");
+console.log("I love", translist[1], "Riding, its always amazing");
+console.log("I love", translist[2], "Riding, its always amazing");
+console.log("I love", translist[3], "Riding, its always amazing");
+console.log("I love", translist[4], "Riding, its always amazing");
