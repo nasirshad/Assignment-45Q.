aaf9f226cb5ecfa730785a88d7e0c3ac3860a0e3
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 21:21:40 +0500

    Assignment-45Q

---
+++ Q17.js
@@ -0,0 +1,6 @@
+"use strict";
+// let sq : string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+// console.log(sq);
+// Question No.17
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+console.log("I have used 13 of my Friend's Names for these exercises");
