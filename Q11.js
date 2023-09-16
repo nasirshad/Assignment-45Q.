2958639110d804be95699ff8ba45d29044727003
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 20:33:19 +0500

    Assignment-45Q

---
+++ Q11.js
@@ -0,0 +1,10 @@
+"use strict";
+//let nam : string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+//console.log(nam);
+//Question No.11
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let namesofFriends = ["'Asif Javeed'", "'Bilal Ahmad'", "'Rizwan Barkat'", "'Ubaid Ullah'"];
+console.log("Hello dear", namesofFriends[0], "Now where are you living now a days");
+console.log("Hello dear", namesofFriends[1], "Now where are you living now a days");
+console.log("Hello dear", namesofFriends[2], "Now where are you living now a days");
+console.log("Hello dear", namesofFriends[3], "Now where are you living now a days");
