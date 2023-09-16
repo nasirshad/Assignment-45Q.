e574cf68d20cb62061d2843302cdad00d623626f
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 22:42:51 +0500

    Assignment-45Q

---
+++ Q34.js
@@ -0,0 +1,8 @@
+"use strict";
+// let saq: string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+// console.log(saq);
+// 34.	T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function..
+function make_shirt(size, txt) {
+    console.log("My Shirt should of size -", size, "- and '", txt, "' should b printed on it");
+}
+make_shirt("Medium", "Zoohan-20");
---
+++ Q34.ts
@@ -0,0 +1,9 @@
+// let saq: string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+// console.log(saq);
+
+// 34.	T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function..
+function make_shirt(size:string,txt:string)
+{
+    console.log("My Shirt should of size -",size,"- and '",txt,"' should b printed on it")
+}
+make_shirt("Medium","Zoohan-20")
