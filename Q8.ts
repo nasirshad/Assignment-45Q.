ed19808178f0f3fd20ae6a4627895933cd41f126
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 19:57:26 +0500

    Assignment-45Q

---
+++ Q8.ts
@@ -0,0 +1,7 @@
+// var nam : string = "MY Name Is Nasir Shad";
+//  console.log(nam);
+
+//Question No.8
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favrtnumber:number=786
+console.log("My Favourite Number is =", favrtnumber);
