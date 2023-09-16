b4ef7dd70f7bf8fa4ba47159c844f458bf21b7f9
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 20:06:38 +0500

    Assignment-45Q

---
+++ Q9.js
@@ -0,0 +1,9 @@
+"use strict";
+//var nam : string = "MY Name Is Nasir Shad";
+//console.log(nam);
+//Question No.9
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+let favrtnumber1 = 786;
+// a variable is declared with favourite number    value
+console.log("My Favourite Number is =", favrtnumber1);
+// taking print of the message and value of the variable stored
