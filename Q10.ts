bacd6f30a21d510b5c3d4405540762f5cf6f29f2
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 20:23:53 +0500

    Assignment-45Q

---
+++ Q10.ts
@@ -0,0 +1,11 @@
+
+//let nam : string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+//console.log(nam);
+
+// Question No.10
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names:string[]=["'Ali Hamza'", "'Azeem'","'Amir Nisar'","'Shauban Arshad'"]
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
