b256542186d023f591cf36654332156d8996d4b5
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 22:08:01 +0500

    Assignment-45Q

---
+++ Q25.js
@@ -0,0 +1,23 @@
+"use strict";
+// let saq: string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+// console.log(saq);
+// Question No.25
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// 	• If the alien is green, print a message that the player earned 5 points.
+// 	• If the alien is yellow, print a message that the player earned 10 points.
+// 	• If the alien is red, print a message that the player earned 15 points.
+// 	• Write three versions of this program, making sure each message is printed for the appropriate color 	alien.
+let alieNColor = "yellow";
+if (alieNColor == "green") {
+    console.log("The Players has Earned 5 Points");
+    console.log("*********");
+}
+//versioin 2
+else if (alieNColor == "yellow") {
+    console.log("The Players has Earned 10 Points");
+    console.log("++++++++");
+}
+else if (alieNColor == "red") {
+    console.log("The Players has Earned 15 Points");
+    console.log("++++++++");
+}
