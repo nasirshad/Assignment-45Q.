5b6032a058dfb96dd14692a369f956840b5ecad3
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 11:56:32 +0500

    Assignment-45Q

---
+++ Q2.ts
@@ -0,0 +1,4 @@
+// Question No.2
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let friendname:string="Junaid Sabri"
+console.log("Hello Dear",friendname,  "Hope u will be very fine");
