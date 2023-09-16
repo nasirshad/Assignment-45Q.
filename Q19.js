736a03d82d5785d4d8e2cf8ae6ad86ddfd7fc66f
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 21:31:18 +0500

    Assignment-45Q

---
+++ Q19.js
@@ -0,0 +1,20 @@
+"use strict";
+let sq = "My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+console.log(sq);
+// Question No.19
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let mydiffPlaces = ["Mount everist,", "River Satluj,", "Rawalpindi,", "Punjabi,", "Pakistan"];
+for (let index = 0; index < mydiffPlaces.length; index++) {
+    const element = mydiffPlaces[index];
+    console.log(element);
+}
+// *think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var An_object_laptop = {
+    Data_Darbar: 'lahore',
+    Keyboard: "Unique",
+    Screen: "36 Inch",
+    Storage: "512 GB",
+    RAM: "16 GB",
+    Processor: "Core i5-7th Gen"
+};
+console.log(An_object_laptop);
