82a427c060b9888563b5127baa43a0e6f9f4b436
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-16 20:16:16 +0500

    Assignment-45Q

---
+++ Q5.js
@@ -0,0 +1,8 @@
+"use strict";
+//let nam : string ="My Name Is Nasir Shad,I Have Started Leraning TypeScript";
+//console.log(nam);
+// Question No.5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let authr = "'Allama Muhammad Iqbal'";
+let quot = "'Nations are born in the hearts of poets,they prosper and die in the hands of politicians.'";
+console.log(authr, ", Once Said ! ", quot);
